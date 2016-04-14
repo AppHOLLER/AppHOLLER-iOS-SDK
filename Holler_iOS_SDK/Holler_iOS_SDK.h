7a5df758eb614dcda5c2f54d8ae32206ab51b290
Phong Nguyen <phong.nguyen@rainmaker-labs.com>
2016-04-14 09:20:38 +0700

    Initial Commit

---
+++ Holler_iOS_SDK/Holler_iOS_SDK.h
@@ -0,0 +1,19 @@
+//
+//  Holler_iOS_SDK.h
+//  Holler_iOS_SDK
+//
+//  Created by Phong Nguyen on 4/14/16.
+//  Copyright © 2016 RML. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Holler_iOS_SDK.
+FOUNDATION_EXPORT double Holler_iOS_SDKVersionNumber;
+
+//! Project version string for Holler_iOS_SDK.
+FOUNDATION_EXPORT const unsigned char Holler_iOS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Holler_iOS_SDK/PublicHeader.h>
+
+
---
+++ Holler_iOS_SDKTests/Holler_iOS_SDKTests.m
@@ -0,0 +1,39 @@
+//
+//  Holler_iOS_SDKTests.m
+//  Holler_iOS_SDKTests
+//
+//  Created by Phong Nguyen on 4/14/16.
+//  Copyright © 2016 RML. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Holler_iOS_SDKTests : XCTestCase
+
+@end
+
+@implementation Holler_iOS_SDKTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
